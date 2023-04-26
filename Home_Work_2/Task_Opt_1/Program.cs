557cd5774186cce4b698105167a11bddf5720f11
KU3MI4 <kuzmichov@bk.ru>
2023-04-26 19:05:21 +0300

    add task optional 1

---
+++ Home_Work_2/Task_Opt_1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача необязательная 1: на входе целое или вещественное число, надо удалить вторую цифру слева этого числа.
+// 145 -> 15
+// 1 -> нет
+// 567,123 -> 57,123
+
+IntNum:
+Console.WriteLine("Введите число:");
+string input = Console.ReadLine();
+double number;
+
+if (double.TryParse(input, out number))
+{
+    int length = input.Length;
+    if (length > 1)
+    {
+        string resultString = "";
+        for (int i = 0; i < length; i++)
+        {
+            if (i != 1)
+            {
+                resultString += input[i];
+            }
+        }
+
+        double resultNumber;
+        if (double.TryParse(resultString, out resultNumber))
+        {
+            Console.WriteLine($"Новое число: {resultNumber}");
+        }       
+    }
+    else
+    {
+        Console.WriteLine("Введённое число однозначное, введите число больше 10");
+        goto IntNum;
+    }
+}
+else
+{
+    goto IntNum;
+}
