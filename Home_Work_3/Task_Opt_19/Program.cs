0c62c9b7e467cca92e3ee37526d119cfee6e9bcf
KU3MI4 <kuzmichov@bk.ru>
2023-04-27 19:50:47 +0300

    add task optional 19

---
+++ Home_Work_3/Task_Opt_19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 19 - HARD необязательная
+// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+
+bool Palindrome(int num1)
+{
+    int rev = 0;
+    int orignum = num1;
+
+    while (num1 > 0)
+    {
+        int digit = num1 % 10;
+        rev = rev * 10 + digit;
+        num1 /= 10;
+    }
+    return (orignum == rev);
+}
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if (Palindrome(num1))
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
