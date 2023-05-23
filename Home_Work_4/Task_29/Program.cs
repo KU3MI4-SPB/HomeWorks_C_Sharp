78033b025ac9fe3de532e5c0f87d8699a6e374ec
KU3MI4 <kuzmichov@bk.ru>
2023-05-23 17:27:40 +0300

    add task 29

---
+++ Home_Work_4/Task_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] CreateArray()
+{
+    Random random = new Random();
+    int[] numbers = new int[8];
+    System.Console.Write("Массив:");
+    for (int x = 0; x < numbers.Length; x++)
+        numbers[x] = random.Next(1, 100);
+    return numbers;
+}
+
+void PrintArray(int [] numbers)
+{
+    for (int x = 0; x < numbers.Length; x++)
+    System.Console.Write($" {numbers[x]}");
+    Console.WriteLine();
+}
+ 
+
+int [] numbers = CreateArray();
+PrintArray(numbers);
