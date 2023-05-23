ac3df306e94eead474bb898fe8e3432afd203c33
KU3MI4 <kuzmichov@bk.ru>
2023-05-23 17:59:27 +0300

    add task optional 26

---
+++ Home_Work_4/Task_Opt_26/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26 HARD Напишите программу, которая принимает на вход целое или дробное число 
+// и выдаёт количество цифр в числе.
+// 456 -> 3
+// 0 -> 1
+// 89,126 -> 5
+// 0,001->4
+
+
+int CountDig(double num)
+{
+     int count = 0;
+     string number = num.ToString();
+     for (int i = 0; i < number.Length; i++)
+     {
+        if (char.IsDigit(number[i]))
+        {
+            count++;
+        }
+     }
+     return count;
+}
+
+
+System.Console.WriteLine("Введите целое или дробное число:");
+double num = double.Parse(Console.ReadLine());
+System.Console.WriteLine($"Количество цифр в цисле {num}: {CountDig(num)}");
