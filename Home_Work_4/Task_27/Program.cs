48f93a0f41a42e7cb1d4783a43c440b4423d9b4d
KU3MI4 <kuzmichov@bk.ru>
2023-05-23 17:00:07 +0300

    add task 27

---
+++ Home_Work_4/Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Summ(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+
+System.Console.WriteLine("Введите любое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {num}, составляет: {Summ(num)}");
