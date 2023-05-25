bb677dddeacad3af42b44f6daf7f90af5b48cd15
KU3MI4 <kuzmichov@bk.ru>
2023-05-25 14:30:10 +0300

    add task 38

---
+++ Home_Work_5/Task_38/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] array = new double[] { 1.2, 2.6, 3.1, 4.9, 5.7 };
+
+double max = array[0];
+double min = array[0];
+
+foreach (double elem in array)
+{
+    if (elem > max)
+        max = elem;
+    if (elem < min)
+        min = elem;
+}
+
+double diff = max - min;
+Console.WriteLine("Разница между максимальным и минимальным элементом массива: " + diff);
