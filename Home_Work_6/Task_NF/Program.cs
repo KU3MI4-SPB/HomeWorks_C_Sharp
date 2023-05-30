847180c9552011e2b6fc14b77b5165469399bafb
KU3MI4 <kuzmichov@bk.ru>
2023-05-30 21:36:21 +0300

    add task NF

---
+++ Home_Work_6/Task_NF/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача НЕГАФИБОНАЧЧИ. Задайте число. Составьте массив чисел НегаФибоначчи, 
+// в том числе для отрицательных индексов.
+// Пример:
+// для k = 9 массив будет выглядеть так: 
+// [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+
+// Console.WriteLine("Задайте число: ");
+// int k = Convert.ToInt32(Console.ReadLine());
+
+// int size = -k + 2; 
+// int[] negaFib = new int[size]; 
+// negaFib[0] = 0;
+// negaFib[1] = -1;
+
+// for (int i = 2; i < size; i++)
+// {
+//     negaFib[i] = negaFib[i - 1] - negaFib[i - 2];
+// }
+
+// Console.WriteLine($"Массив чисел НегаФибоначчи для K = {k}:");
+// foreach (int n in negaFib)
+// {
+//     Console.Write($"{n} ");
+// }
+
