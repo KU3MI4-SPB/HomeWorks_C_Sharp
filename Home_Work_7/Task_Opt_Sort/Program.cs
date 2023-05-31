33a2241a7c355977b34a0f4fe86a8642d4154a11
KU3MI4 <kuzmichov@bk.ru>
2023-06-01 00:38:48 +0300

    add task opt sort

---
+++ Home_Work_7/Task_Opt_Sort/Program.cs
@@ -0,0 +1,73 @@
+﻿// Задача HARD SORT необязательная. Считается за три обязательных
+// Задайте двумерный массив из целых чисел. Количество строк и столбцов 
+// задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 10 3
+
+// После сортировки
+// 1 2 3 4
+// 5 7 9 10
+
+
+void FillArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0);i++)
+        for (int j=0; j<array.GetLength(1);j++)
+            array[i,j] = new Random().Next(1,30);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0; i<array.GetLength(0);i++)
+        {
+        for (int j=0; j<array.GetLength(1);j++)
+            System.Console.Write($"{array[i,j],3}    ");
+        System.Console.WriteLine();
+        }
+}
+
+void SortArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int i = 0; i < rows; i++)
+    {
+        int[] rowArray = new int[cols];
+        for (int j = 0; j < cols; j++)
+            rowArray[j] = array[i, j];
+        Array.Sort(rowArray);
+        for (int j = 0; j < cols; j++)
+            array[i, j] = rowArray[j];
+    }
+}
+
+void SortColumns(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int cols = array.GetLength(1);
+    for (int j = 0; j < cols; j++)
+    {
+        int[] colArray = new int[rows];
+        for (int i = 0; i < rows; i++)
+            colArray[i] = array[i, j];
+        Array.Sort(colArray);
+        for (int i = 0; i < rows; i++)
+            array[i, j] = colArray[i];
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+SortArray(array);
+SortColumns(array);
+Console.WriteLine("Отсортированный массив:");
+PrintArray(array);
